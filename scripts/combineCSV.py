9c075b3bd65cd268579967101776463eb31816d5
Katherine Evans <ktevans@email.wm.edu>
2024-04-23 15:33:20 -0400

    setup scripts added

---
+++ scripts/combineCSV.py
@@ -0,0 +1,20 @@
+#import modules
+import os
+import pandas as pd
+
+#This script will combine all CSV files in a directory into a new file in that same directory called "combined.csv".
+
+#Define the path to the CSV files and an output file for the combined CSV file.
+file_path = "../output/Symmetric/Pass2_Optics1/"
+output_file = file_path + "combined.csv"
+
+#Make a list of all files in a directory.
+file_list = os.listdir(file_path)
+#print(file_list)
+
+#Combine data from all files in file_list into one data frame.
+df_concat = pd.concat([pd.read_csv(file_path+f) for f in file_list], ignore_index=True)
+
+#Write the data frame to a CSV file.
+df_concat.to_csv(output_file)
+
---
+++ sourceme.csh
@@ -0,0 +1,2 @@
+source venv/bin/activate.csh
+alias python3 venv/bin/python3
